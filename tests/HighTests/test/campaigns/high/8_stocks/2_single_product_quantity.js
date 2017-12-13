7b89e71d29e472dbd0371e7d8d1f3f3f11bff88f
fouratachour <fourat.ach@gmail.com>
2017-12-13 17:39:03 +0100

    Code Review

--- tests/HighTests/test/campaigns/high/8_stocks/2_single_product_quantity.js
+++ tests/HighTests/test/campaigns/high/8_stocks/2_single_product_quantity.js
@@ -1,18 +1,43 @@
 const {AccessPageBO} = require('../../../selectors/BO/access_page');
+const {CatalogPage} = require('../../../selectors/BO/catalogpage/index');
+const {Stock} = require('../../../selectors/BO/catalogpage/stocksubmenu/stock');
+const {Movement} = require('../../../selectors/BO/catalogpage/stocksubmenu/movements');
+
 scenario('Modify quantity and check movement for single product', client => {
-  test('should open the browser', () => client.open());
-  test('should sign in the Back Office', () => client.signInBO(AccessPageBO));
-  test('should go to catalog stocks', () => client.goToCatalogStock());
-  test('should get quantity of the third product', () => client.getThirdProductQuantity());
-  test('should change third product quantity', () => client.modifyThirdProductQuantity());
-  test('should save third product quantity', () => client.saveThirdProduct());
-  test('should go to movements tab', () => client.goToStockMovements());
-  test('should check movement history', () => client.checkMovement(1, "3", "+", "Employee Edition"));
-  test('should go to stock tab', () => client.goToStock());
-  test('should get fourth product quantity', () => client.getFourthProductQuantity());
-  test('should change fourth product quantity', () => client.modifyFourthProductQuantity());
-  test('should save fourth product quantity', () => client.saveFourthProduct());
-  test('should go to movements tab', () => client.goToStockMovements());
-  test('should check movement history', () => client.checkMovement(1, "3", "-", "Employee Edition"));
-  test('should sign Out the Back Office', () => client.signOutBO());
+  scenario('Login in the Back Office', client => {
+    test('should open the browser', () => client.open());
+    test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+  }, 'stocks');
+  scenario('Modify quantity and check movement for single product', client => {
+    test('should go to "Stocks" page', () => client.goToSubtabMenuPage(CatalogPage.menu_button, Stock.submenu));
+    test('should get the "Quantity" of the third product', () => client.getTextInVar(Stock.product_quantity.replace('%O', 3), "productQuantity"));
+    test('should change the third product quantity', () => {
+      let promise = Promise.resolve();
+      promise = client.moveToObject(Stock.product_quantity_input.replace('%O', 3));
+      for (let i = 1; i < 4; i++) {
+        promise = client.waitForExistAndClick(Stock.add_quantity_button);
+      }
+      return promise
+        .then(() => client.getTextInVar(Stock.product_quantity.replace('%O', 3), "productQuantity"))
+        .then(() => client.checkTextValue(Stock.product_quantity_modified.replace('%O', 3), global.tab["productQuantity"].substring(18), "contain"));
+    });
+    test('should click on "Check" button of the third product quantity', () => client.waitForExistAndClick(Stock.save_product_quantity_button.replace('%O', 3)));
+    test('should go to "Movements" tab', () => client.goToStockMovements(Movement));
+    test('should check movement history', () => client.checkMovement(Movement, 1, "3", "+", "Employee Edition"));
+    test('should go to "Stock" tab', () => client.waitForExistAndClick(Stock.tabs));
+    test('should get the fourth product quantity', () => client.getTextInVar(Stock.product_quantity.replace('%O', 4), "productQuantity"));
+    test('should change the fourth product quantity', () => {
+      let promise = Promise.resolve();
+      promise = client.moveToObject(Stock.product_quantity_input.replace('%O', 4));
+      for (let i = 1; i < 4; i++) {
+        promise = client.waitForExistAndClick(Stock.remove_quantity_button);
+      }
+      return promise
+        .then(() => client.getTextInVar(Stock.product_quantity.replace('%O', 4), "productQuantity"))
+        .then(() => client.checkTextValue(Stock.product_quantity_modified.replace('%O', 4), global.tab["productQuantity"].substring(18), "contain"));
+    });
+    test('should click on "Check" button of fourth product quantity', () => client.waitForExistAndClick(Stock.save_product_quantity_button.replace('%O', 4)));
+    test('should go to "Movements" tab', () => client.goToStockMovements(Movement));
+    test('should check movement history', () => client.checkMovement(Movement, 1, "3", "-", "Employee Edition"));
+  }, 'stocks');
 }, 'stocks', true);
--- tests/HighTests/test/selectors/BO/catalogpage/stocksubmenu/movements.js
+++ tests/HighTests/test/selectors/BO/catalogpage/stocksubmenu/movements.js
@@ -1,6 +1,9 @@
 module.exports = {
   Movement:{
     tabs: '//*[@id="tab"]/li[2]/a',
-    variation: '//*[@id="app"]/div[3]/section/table/tbody/tr[1]/td[4]/span/span'
+    variation: '//*[@id="app"]/div[3]/section/table/tbody/tr[1]/td[4]/span/span',
+    variation_value:'//*[@id="app"]/div[3]/section/table/tbody/tr[%P]/td[4]/span/span',
+    quantity_value:'//*[@id="app"]/div[3]/section/table/tbody/tr[%P]/td[4]/span',
+    type_value:'//*[@id="app"]/div[3]/section/table/tbody/tr[%P]/td[3]',
   }
 };
